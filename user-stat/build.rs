b467f17f122c71a7fea51fd022a2e2084a06448c
tr1um7h <tr1um7h@gmail.com>
2026-03-15 08:41:07 +0800

    update code: with_serde is obsolete. TODO: proto-builder-trait 更新支持 tonic-prost-build

--- user-stat/build.rs
+++ user-stat/build.rs
@@ -3,14 +3,19 @@ use std::fs;
 
 fn main() -> Result<()> {
     fs::create_dir_all("src/pb")?;
-    let config = tonic_prost_build::configure();
-    config.out_dir("src/pb").compile_protos(
-        &[
-            "../protos/user_stats/messages.proto",
-            "../protos/user_stats/rpc.proto",
-        ],
-        &["../protos"],
-    )?;
+
+    tonic_prost_build::configure()
+        .out_dir("src/pb")
+        // 为 User 消息添加 serde 属性
+        .type_attribute("User", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute("User", r#"#[serde(rename_all = "camelCase")]"#)
+        .compile_protos(
+            &[
+                "../protos/user_stats/messages.proto",
+                "../protos/user_stats/rpc.proto",
+            ],
+            &["../protos"],
+        )?;
 
     println!("cargo:rerun-if-changed=../protos/user_stats/*.proto");
 
