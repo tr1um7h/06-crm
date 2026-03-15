efdd9274906f0d53f68042c45d88697f8883b15a
tr1um7h <tr1um7h@gmail.com>
2026-03-15 11:09:01 +0800

    feature: add user-stat service skeleton

--- user-stat/src/lib.rs
+++ user-stat/src/lib.rs
@@ -1,1 +1,40 @@
 pub mod pb;
+
+use futures::Stream;
+use pb::{
+    QueryRequest, RawQueryRequest, User,
+    user_stats_server::{UserStats, UserStatsServer},
+};
+use std::pin::Pin;
+use tonic::{Request, Response, Status, async_trait};
+
+#[derive(Default)]
+pub struct UserStatsService {}
+
+type ServiceResult<T> = Result<Response<T>, Status>;
+
+// Stream: futures::Stream
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<User, Status>> + Send>>;
+
+#[async_trait]
+impl UserStats for UserStatsService {
+    type QueryStream = ResponseStream;
+    type RawQueryStream = ResponseStream;
+
+    async fn query(&self, _request: Request<QueryRequest>) -> ServiceResult<Self::QueryStream> {
+        unimplemented!()
+    }
+
+    async fn raw_query(
+        &self,
+        _request: Request<RawQueryRequest>,
+    ) -> ServiceResult<Self::RawQueryStream> {
+        unimplemented!()
+    }
+}
+
+impl From<UserStatsService> for UserStatsServer<UserStatsService> {
+    fn from(svc: UserStatsService) -> Self {
+        UserStatsServer::new(svc)
+    }
+}
--- user-stat/src/main.rs
+++ user-stat/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tonic::transport::Server;
+use user_stat::{UserStatsService, pb::user_stats_server::UserStatsServer};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr = "[::1]:50051".parse().expect("should parse addr ok");
+    let svc = UserStatsServer::from(UserStatsService::default());
+
+    println!("UserStatsServer is listening to {}", addr);
+
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
 }
