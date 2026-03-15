c01485ccb6fc3f55585079dad4a206c8e8ee5430
tr1um7h <tr1um7h@gmail.com>
2026-03-15 10:34:05 +0800

    refactor: migrate to tonic-prost-build and ignore generated pb files

--- user-stat/build.rs
+++ user-stat/build.rs
@@ -9,6 +9,18 @@ fn main() -> Result<()> {
         // 为 User 消息添加 serde 属性
         .type_attribute("User", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute("User", r#"#[serde(rename_all = "camelCase")]"#)
+        // 为 User 消息添加 Builder 派生宏
+        .type_attribute("User", "#[derive(derive_builder::Builder)]")
+        .type_attribute("User", "#[builder(setter(into, strip_option), default)]")
+        .type_attribute("QueryRequest", "#[derive(derive_builder::Builder)]")
+        .type_attribute("RawQueryRequest", "#[derive(derive_builder::Builder)]")
+        .type_attribute("TimeQuery", "#[derive(derive_builder::Builder)]")
+        .type_attribute("IdQuery", "#[derive(derive_builder::Builder)]")
+        .field_attribute("User.email", "#[builder(setter(into))]")
+        .field_attribute("User.name", "#[builder(setter(into))]")
+        .field_attribute("RawQueryRequest.query", "#[builder(setter(into))]")
+        .field_attribute("TimeQuery.lower", "#[builder(setter(into, strip_option))]")
+        .field_attribute("TimeQuery.upper", "#[builder(setter(into, strip_option))]")
         .compile_protos(
             &[
                 "../protos/user_stats/messages.proto",
