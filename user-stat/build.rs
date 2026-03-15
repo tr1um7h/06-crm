5f724ce158130d05e69246856ebd9c67bf4f1f72
tr1um7h <tr1um7h@gmail.com>
2026-03-15 14:18:16 +0800

    [chore] update proto-builder-trait, use builder annotation

--- user-stat/build.rs
+++ user-stat/build.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use proto_builder_trait::tonic_prost::BuilderAttributes; // 添加这一行
 use std::fs;
 
 fn main() -> Result<()> {
@@ -6,21 +7,39 @@ fn main() -> Result<()> {
 
     tonic_prost_build::configure()
         .out_dir("src/pb")
-        // 为 User 消息添加 serde 属性
-        .type_attribute("User", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .type_attribute("User", r#"#[serde(rename_all = "camelCase")]"#)
-        // 为 User 消息添加 Builder 派生宏
-        .type_attribute("User", "#[derive(derive_builder::Builder)]")
-        .type_attribute("User", "#[builder(setter(into, strip_option), default)]")
-        .type_attribute("QueryRequest", "#[derive(derive_builder::Builder)]")
-        .type_attribute("RawQueryRequest", "#[derive(derive_builder::Builder)]")
-        .type_attribute("TimeQuery", "#[derive(derive_builder::Builder)]")
-        .type_attribute("IdQuery", "#[derive(derive_builder::Builder)]")
-        .field_attribute("User.email", "#[builder(setter(into))]")
-        .field_attribute("User.name", "#[builder(setter(into))]")
-        .field_attribute("RawQueryRequest.query", "#[builder(setter(into))]")
-        .field_attribute("TimeQuery.lower", "#[builder(setter(into, strip_option))]")
-        .field_attribute("TimeQuery.upper", "#[builder(setter(into, strip_option))]")
+        .with_serde(
+            &["User"],
+            true,
+            true,
+            Some(&[r#"#[serde(rename_all = "camelCase")]"#]),
+        )
+        .with_sqlx_from_row(&["User"], None)
+        .with_derive_builder(
+            &[
+                "User",
+                "QueryRequest",
+                "RawQueryRequest",
+                "TimeQuery",
+                "IdQuery",
+            ],
+            None,
+        )
+        .with_field_attributes(
+            &["User.email", "User.name", "RawQueryRequest.query"],
+            &[r#"#[builder(setter(into))]"#],
+        )
+        .with_field_attributes(
+            &["TimeQuery.before", "TimeQuery.after"],
+            &[r#"#[builder(setter(into, strip_option))]"#],
+        )
+        .with_field_attributes(
+            &["QueryRequest.timestamps"],
+            &[r#"#[builder(setter(each(name="timestamp", into)))]"#],
+        )
+        .with_field_attributes(
+            &["QueryRequest.ids"],
+            &[r#"#[builder(setter(each(name="id", into)))]"#],
+        )
         .compile_protos(
             &[
                 "../protos/user_stats/messages.proto",
